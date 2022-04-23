d1cb9c306f951acdab3da579d9d5b1cd2a77c2e8
GrayNeel <paoul32@yahoo.it>
2022-04-23 16:10:22 +0200

    feat: menu item on show offer

--- app/src/main/java/it/polito/mad/g01_timebanking/ui/timeslotdetails/TimeSlotDetailsFragment.kt
+++ app/src/main/java/it/polito/mad/g01_timebanking/ui/timeslotdetails/TimeSlotDetailsFragment.kt
@@ -1,12 +1,13 @@
 package it.polito.mad.g01_timebanking.ui.timeslotdetails
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import it.polito.mad.g01_timebanking.R
 import it.polito.mad.g01_timebanking.databinding.FragmentTimeSlotDetailsBinding
 
 
@@ -33,11 +34,32 @@ class TimeSlotDetailsFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             //textView.text = it
         }
+
+        // Enable menu options
+        setHasOptionsMenu(true)
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        /**
+         * The following code displays the fragment that
+         * has the same ID as the menu item. (This code only works if the menu item
+         * and the fragment have identical ID values.)
+         */
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
