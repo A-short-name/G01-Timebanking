1b149563debd14bfaf8627ac5baf5143e3be5992
GrayNeel <paoul32@yahoo.it>
2022-04-23 16:44:53 +0200

    feat: date picker added

--- app/src/main/java/it/polito/mad/g01_timebanking/ui/timeslotedit/TimeSlotEditFragment.kt
+++ app/src/main/java/it/polito/mad/g01_timebanking/ui/timeslotedit/TimeSlotEditFragment.kt
@@ -1,14 +1,25 @@
 package it.polito.mad.g01_timebanking.ui.timeslotedit
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import it.polito.mad.g01_timebanking.R
 import it.polito.mad.g01_timebanking.databinding.FragmentTimeSlotEditBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class TimeSlotEditFragment : Fragment() {
+    /** Vars for Date Picker **/
+    private val calendar: Calendar = Calendar.getInstance()
+    private lateinit var editText: EditText
 
     private var _binding: FragmentTimeSlotEditBinding? = null
 
@@ -21,11 +32,15 @@ class TimeSlotEditFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val galleryViewModel =
-            ViewModelProvider(this).get(TimeSlotEditViewModel::class.java)
+            ViewModelProvider(this)[TimeSlotEditViewModel::class.java]
 
         _binding = FragmentTimeSlotEditBinding.inflate(inflater, container, false)
+
         val root: View = binding.root
+
+        editText = root.findViewById(R.id.editTextDate)
 
         return root
     }
@@ -33,5 +48,32 @@ class TimeSlotEditFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val date = OnDateSetListener { _, year, month, day ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, day)
+                updateLabel()
+        }
+
+        editText.setOnClickListener {
+            DatePickerDialog(
+                this.requireContext(),
+                date,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
+    private fun updateLabel() {
+        val myFormat = "dd/MM/yyyy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        editText.setText(dateFormat.format(calendar.time))
     }
 }
