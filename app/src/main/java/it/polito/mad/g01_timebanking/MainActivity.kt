5809724a3f68c045c63bd840b74e3cdf9bcf8a10
GrayNeel <paoul32@yahoo.it>
2022-05-03 00:12:26 +0200

    fix: warnings

--- app/src/main/java/it/polito/mad/g01_timebanking/MainActivity.kt
+++ app/src/main/java/it/polito/mad/g01_timebanking/MainActivity.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.os.bundleOf
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
