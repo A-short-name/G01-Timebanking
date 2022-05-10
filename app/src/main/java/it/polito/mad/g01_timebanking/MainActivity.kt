a7c3a1bf93992488fe759bf4536daba18cfeb399
GrayNeel <paoul32@yahoo.it>
2022-05-10 14:41:54 +0200

    feat: added activity_login.xml #35

--- app/src/main/java/it/polito/mad/g01_timebanking/MainActivity.kt
+++ app/src/main/java/it/polito/mad/g01_timebanking/MainActivity.kt
@@ -1,5 +1,3 @@
-@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-
 package it.polito.mad.g01_timebanking
 
 import android.content.ContentValues.TAG
@@ -40,15 +38,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
-    private lateinit var signUpRequest: BeginSignInRequest
-
-    private val REQ_ONE_TAP = 2
-
-    private lateinit var auth: FirebaseAuth
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -122,84 +111,6 @@ class MainActivity : AppCompatActivity() {
             if (destination.id==R.id.nav_edit_profile || destination.id==R.id.nav_edit_time_slot)
                 findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar).navigationIcon= null
         }
-
-
-        oneTapSignIn()
-    }
-
-    private fun oneTapSignIn() {
-        /* Building sign-up request code */
-        oneTapClient = Identity.getSignInClient(this)
-        signUpRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.g01_web_client_id))
-                    // Show all accounts on the device.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .build()
-
-        /* Building Google Sign-in code */
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(
-                BeginSignInRequest.PasswordRequestOptions.builder()
-                    .setSupported(true)
-                    .build()
-            )
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.g01_web_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build()
-            )
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
-            .build()
-
-        /* Starting sign-in request */
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(this) { result ->
-                try {
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender, REQ_ONE_TAP,
-                        null, 0, 0, 0, null
-                    )
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-            }
-            .addOnFailureListener(this) { e ->
-                // No saved credentials found. Launch the One Tap sign-up flow, or
-                // do nothing and continue presenting the signed-out UI.
-
-                /* Starting sign-up flow */
-                Log.d(TAG, e.localizedMessage)
-                oneTapClient.beginSignIn(signUpRequest)
-                    .addOnSuccessListener(this) { result ->
-                        try {
-                            Log.d(TAG, "Starting sign-up request")
-                            startIntentSenderForResult(
-                                result.pendingIntent.intentSender, REQ_ONE_TAP,
-                                null, 0, 0, 0
-                            )
-                        } catch (e: IntentSender.SendIntentException) {
-                            Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                        }
-                    }
-                    .addOnFailureListener(this) { _ ->
-                        // No Google Accounts found. Just continue presenting the signed-out UI.
-                        // Signed-out UI is unwanted so the app is closed
-                        Toast.makeText(this,"Google account is necessary to use the app. Add at least one to your device",Toast.LENGTH_SHORT).show()
-                        finish()
-                        Log.d(TAG, e.localizedMessage)
-                    }
-            }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -222,58 +133,6 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        auth = Firebase.auth
-
-        when (requestCode) {
-            REQ_ONE_TAP -> {
-                try {
-                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
-                    val idToken = credential.googleIdToken
-                    val username = credential.id
-                    val password = credential.password
-                    when {
-                        idToken != null -> {
-                            // Got an ID token from Google. Use it to authenticate
-                            // with firebase
-
-                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-                            auth.signInWithCredential(firebaseCredential)
-                                .addOnCompleteListener(this) { task ->
-                                    if (task.isSuccessful) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d(TAG, "signInWithCredential:success")
-                                        val user = auth.currentUser
-                                        //updateUI(user)
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        Log.w(TAG, "signInWithCredential:failure", task.exception)
-                                        //updateUI(null)
-                                    }
-                                }
-
-                            Log.d(TAG, "Got ID token.")
-                        }
-                        password != null -> {
-                            // Got a saved username and password. Use them to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got password.")
-                        }
-                        else -> {
-                            // Shouldn't happen.
-                            Log.d(TAG, "No ID token or password!")
-                        }
-                    }
-                } catch (e: ApiException) {
-                    Toast.makeText(this,"Google account is necessary to use the app.",Toast.LENGTH_SHORT).show()
-                    finish()
-                    Log.d(TAG, "Api exception thrown from activityResult: ${e.message}")
-                }
-            }
-        }
     }
 
 }
